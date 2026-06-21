020a8cd1cfc25357953ee5bf90f0673a46d2479f
pskabdulkadir <psikologabdulkadirkan@gmail.com>
2026-06-21 23:56:57 +0300

    Add serve package

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(join(__dirname, 'dist', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
